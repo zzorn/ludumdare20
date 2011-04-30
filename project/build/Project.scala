192fe2d9449e3ada7e645e32de00d8f381a75c11
zzorn <zzorn@iki.fi>
2011-04-30 10:47:11 +0300

    SBT project build files

---
+++ project/build/Project.scala
@@ -0,0 +1,31 @@
+import sbt._
+
+class Project(info: ProjectInfo) extends DefaultProject(info) with AkkaProject with IdeaProject
+{
+  // Scala unit testing
+  val scalatest = "org.scalatest" % "scalatest" % "1.3"
+
+  // Store all JME3 jar files in own directory under lib/
+  def jme3Jars = descendents("lib" / "jme3", "*.jar")
+
+  // Simplex libs
+  def simplex3dJars = descendents("lib" / "simplex3d", "*.jar")
+
+  override def unmanagedClasspath = super.unmanagedClasspath +++ jme3Jars +++ simplex3dJars
+
+/*
+  // Akka libraries, uncomment if / as needed. 
+  val akkaStm = akkaModule("stm")
+  val akkaTypedActor = akkaModule("typed-actor")
+  val akkaRemote = akkaModule("remote")
+  val akkaHttp = akkaModule("http")
+  val akkaAmqp = akkaModule("amqp")
+  val akkaCamel = akkaModule("camel")
+  val akkaSpring = akkaModule("spring")
+  val akkaJta = akkaModule("jta")
+  val akkaCassandra = akkaModule("persistence-cassandra")
+  val akkaMongo = akkaModule("persistence-mongo")
+  val akkaRedis = akkaModule("persistence-redis")
+*/
+  
+}
